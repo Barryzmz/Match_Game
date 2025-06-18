6023e5f443ef3179d0c49be778e8d28d42ccff8a
BarryZhuang <1007koi@gmail.com>
2025-06-19 00:54:20 +0800

    feat: 新增SetUpGame()

--- Match_Game/MainWindow.xaml.cs
+++ Match_Game/MainWindow.xaml.cs
@@ -19,5 +19,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SetUpGame();
+    }
+
+    private void SetUpGame()
+    {
+        List<string> animalEmoji = new List<string>()
+        {
+            "🐘", "🐘",
+            "🦅", "🦅",
+            "🦘", "🦘",
+            "🦑", "🦑",
+            "🦁", "🦁",
+            "🐨", "🐨",
+            "🐒", "🐒",
+            "🐳", "🐳",
+        };
+        Random random = new Random();
+        foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+        {
+            int index = random.Next(animalEmoji.Count);
+            string nextEmoji = animalEmoji[index];
+            textBlock.Text = nextEmoji;
+            animalEmoji.RemoveAt(index);
+        }
     }
 }
