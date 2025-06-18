189dbfb213b0fd072558dc89bdf225e83bbb7d7e
BarryZhuang <1007koi@gmail.com>
2025-06-19 01:18:33 +0800

    feat: 新增TextBlock_MouseDown處理滑鼠點擊事件

--- Match_Game/MainWindow.xaml.cs
+++ Match_Game/MainWindow.xaml.cs
@@ -16,6 +16,10 @@ namespace Match_Game;
 /// </summary>
 public partial class MainWindow : Window
 {
+    // 記錄玩家按的第一個動物
+    TextBlock lastTextBlockClicked;
+    // 記錄玩家是否已經按了第一個動物
+    bool findingMatch = false;
     public MainWindow()
     {
         InitializeComponent();
@@ -42,6 +46,30 @@ public partial class MainWindow : Window
             string nextEmoji = animalEmoji[index];
             textBlock.Text = nextEmoji;
             animalEmoji.RemoveAt(index);
+        }
+    }
+
+    private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        TextBlock textBlock = sender as TextBlock;
+        if (findingMatch == false)
+        {
+            // 第一次按，將第一個動物隱藏，並記錄TextBox
+            textBlock.Visibility = Visibility.Hidden;
+            lastTextBlockClicked = textBlock;
+            findingMatch = true;
+        }
+        else if (textBlock.Text == lastTextBlockClicked.Text)
+        {
+            // 配對成功，並也把第二個動物隱藏，然後重設findingMatch 
+            textBlock.Visibility = Visibility.Hidden;
+            findingMatch = false;
+        }
+        else
+        {
+            // 配對失敗，把第一個動物顯示，然後重設findingMatch 
+            lastTextBlockClicked.Visibility = Visibility.Visible;
+            findingMatch = false;
         }
     }
 }
